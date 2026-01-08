99c5a80b52258ab14642d3ec39d30860948d925e
Amanuel Fisseha <amanuelfisseha2@gmail.com>
2026-01-08 03:13:15 +0300

    refactor: update Notification interface to support optional title and broadcastId

--- src/types/notification.type.ts
+++ src/types/notification.type.ts
@@ -3,8 +3,10 @@ import { Document, Types } from 'mongoose'
 export interface INotification extends Document {
   _id: Types.ObjectId
   userId: Types.ObjectId
+  title?: string;
   type: string
   message: string
   isRead: boolean
+  broadcastId?: Types.ObjectId | null; 
   createdAt: Date
 }
