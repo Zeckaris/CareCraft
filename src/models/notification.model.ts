812060d211aeed07198fe4bbaf4f42f77b504969
Amanuel Fisseha <amanuelfisseha2@gmail.com>
2026-01-09 21:39:26 +0300

    refactor: Update notification model to support title and broadcast message id

--- src/models/notification.model.ts
+++ src/models/notification.model.ts
@@ -5,8 +5,10 @@ const notificationSchema = new Schema<INotification>(
   {
     userId: { type: Schema.Types.ObjectId, ref: 'UserAccount', required: true },
     type: { type: String, required: true, trim: true },
+     title: { type: String, trim: true },
     message: { type: String, required: true },
-    isRead: { type: Boolean, default: false }
+    isRead: { type: Boolean, default: false },
+    broadcastId: { type: Schema.Types.ObjectId, ref: 'BroadcastMessage', default: null }
   },
   {
     timestamps: { createdAt: 'createdAt', updatedAt: false }
